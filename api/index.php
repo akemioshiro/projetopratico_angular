e8018eba9218afd80dd965cefb5f68b2d039ff18
akemioshiro@hotmail.com <akemioshiro@hotmail.com>
2017-04-03 22:38:27 -0300

    alterar noticia no mesmo formulario

--- api/index.php
+++ api/index.php
@@ -15,56 +15,99 @@ header("Content-Type: application/json");
 $app->post(
     '/login',
     function () use ($app) {
-
+        
         $data = json_decode($app->request()->getBody());
         $usuario = (isset($data->usuario)) ? $data->usuario : "";
 	    $senha   = (isset($data->senha)) ? $data->senha : "";
-
+        
         if($usuario=="admin" && $senha=="123456"){
-
+            
             $_SESSION['logado']=true;
-
+            
             echo json_encode(array("logado"=>true));
         } else {
-            echo json_encode(array("logado"=>false));
+            echo json_encode(array("logado"=>false));   
         }
-
+        
     }
 );
 
 $app->post('/cadastrarNovaNoticia', 'auth', function () use ($app, $db) {
-
+        
         $data = json_decode($app->request()->getBody());
         $noticiatitulo = (isset($data->noticiatitulo)) ? $data->noticiatitulo : "";
 	    $noticiadescricao = (isset($data->noticiadescricao)) ? $data->noticiadescricao : "";
         $noticiadata = (isset($data->noticiadata)) ? $data->noticiadata : "";
         $noticiatexto = (isset($data->noticiatexto)) ? $data->noticiatexto : "";
-
+        
         $data_tmp = explode('/',$noticiadata);
-
+    
         if(checkdate($data_tmp[1], $data_tmp[0], $data_tmp[2])){
             $data = sprintf('%s-%s-%s', $data_tmp[2], $data_tmp[1], $data_tmp[0]);
         } else {
-            $data = NULL;
+            $data = NULL; 
         }
-
+        
         $consulta = $db->con()->prepare('INSERT INTO noticia(noticiatitulo, noticiadescricao, noticiatexto, noticiadata) VALUES (:NOTICIATITULO, :NOTICIADESCRICAO, :NOTICIATEXTO, :NOTICIADATA)');
         $consulta->bindParam(':NOTICIATITULO', $noticiatitulo);
         $consulta->bindParam(':NOTICIADESCRICAO', $noticiadescricao);
         $consulta->bindParam(':NOTICIATEXTO', $noticiatexto);
         $consulta->bindParam(':NOTICIADATA', $data);
-
+    
         if($consulta->execute()){
             echo json_encode(array("erro"=>false));
         } else {
             echo json_encode(array("erro"=>true));
         }
+        
+    }
+);
 
+$app->post('/alterarNoticia/:idnoticia', 'auth', function ($idnoticia) use ($app, $db) {
+        
+        $data = json_decode($app->request()->getBody());
+    
+        $idnoticia = (int)$idnoticia;
+    
+        $noticiatitulo = (isset($data->noticiatitulo)) ? $data->noticiatitulo : "";
+	    $noticiadescricao = (isset($data->noticiadescricao)) ? $data->noticiadescricao : "";
+        $noticiadata = (isset($data->noticiadata)) ? $data->noticiadata : "";
+        $noticiatexto = (isset($data->noticiatexto)) ? $data->noticiatexto : "";
+        
+        $data_tmp = explode('/',$noticiadata);
+    
+        if(checkdate($data_tmp[1], $data_tmp[0], $data_tmp[2])){
+            $data = sprintf('%s-%s-%s', $data_tmp[2], $data_tmp[1], $data_tmp[0]);
+        } else {
+            $data = NULL; 
+        }
+        
+        $consulta = $db->con()->prepare('UPDATE noticia 
+                                        SET 
+                                            noticiatitulo = :NOTICIATITULO, 
+                                            noticiadescricao = :NOTICIADESCRICAO, 
+                                            noticiatexto = :NOTICIATEXTO, 
+                                            noticiadata = :NOTICIADATA
+                                        WHERE 
+                                            idnoticia = :IDNOTICIA');
+    
+        $consulta->bindParam(':NOTICIATITULO', $noticiatitulo);
+        $consulta->bindParam(':NOTICIADESCRICAO', $noticiadescricao);
+        $consulta->bindParam(':NOTICIATEXTO', $noticiatexto);
+        $consulta->bindParam(':NOTICIADATA', $data);
+        $consulta->bindParam(':IDNOTICIA', $idnoticia);
+    
+        if($consulta->execute()){
+            echo json_encode(array("erro"=>false));
+        } else {
+            echo json_encode(array("erro"=>true));
+        }
+        
     }
 );
 
 $app->get('/listarNoticias', 'auth', function () use ($app, $db) {
-
+            
         $consulta = $db->con()->prepare("SELECT
                                             idnoticia,
                                             noticiatitulo,
@@ -80,7 +123,32 @@ $app->get('/listarNoticias', 'auth', function () use ($app, $db) {
         $consulta->execute();
         $noticias = $consulta->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode(array("noticias"=>$noticias));
+        
+    }
+);
 
+$app->get('/getnoticia/:idnoticia', 'auth', function ($idnoticia) use ($app, $db) {
+        $idnoticia = (int)$idnoticia;
+    
+        $consulta = $db->con()->prepare("SELECT
+                                            idnoticia,
+                                            noticiatitulo,
+                                            noticiadescricao,
+                                            noticiatexto,
+                                            DATE_FORMAT(noticiadata,'%d/%m/%Y') AS noticiadata
+                                        FROM
+                                            noticia
+                                        WHERE
+                                            idnoticia = :IDNOTICIA                                            
+                                        ORDER BY
+                                            noticiadata DESC,
+                                            noticiatitulo ASC
+                                        ");
+        $consulta->bindParam(':IDNOTICIA', $idnoticia);
+        $consulta->execute();
+        $noticias = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode(array("noticia"=>$noticias[0]));
+        
     }
 );
 
--- painel/index.php
+++ painel/index.php
@@ -1,4 +1,4 @@
-﻿<html ng-app="app">
+<html ng-app="app">
     <head>
         <title>Painel Administrativo - Login</title>
         <meta charset="utf-8">
@@ -53,7 +53,7 @@
                 </div>
                 
             </div>
-             
+            
             
         </div>
         
